23918700a4eecadc8d0c8f0a533109f9ce7da4da
Kuba Suder <jakub.suder@gmail.com>
2016-02-15 00:01:41 +0100

    updated git repository url

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'bundler/capistrano'
 
 set :application, "twilio"
-set :repository, "git@github.com:jsuder/twilio-sms-to-email.git"
+set :repository, "git@github.com:mackuba/twilio-sms-to-email.git"
 set :scm, :git
 set :keep_releases, 5
 set :use_sudo, false
